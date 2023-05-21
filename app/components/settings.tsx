15134480fb9a0ba215bb26fd2e69ab8066a7ee0c
chenxiaoming <732803261@qq.com>
2023-05-21 23:32:15 +0800

    annotation code 注释掉github入口，版本更新，余额查询，模型选择,修改ChatGPT Next 为ChatGPT

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -355,7 +355,7 @@ export function Settings() {
             </Popover>
           </ListItem>
 
-          <ListItem
+          {/*          <ListItem
             title={Locale.Settings.Update.Version(currentVersion ?? "unknown")}
             subTitle={
               checkingUpdate
@@ -378,7 +378,7 @@ export function Settings() {
                 onClick={() => checkUpdate(true)}
               />
             )}
-          </ListItem>
+          </ListItem>*/}
 
           <ListItem title={Locale.Settings.SendKey}>
             <Select
@@ -518,7 +518,7 @@ export function Settings() {
             </ListItem>
           ) : null}
 
-          <ListItem
+          {/*<ListItem
             title={Locale.Settings.Usage.Title}
             subTitle={
               showUsage
@@ -540,7 +540,7 @@ export function Settings() {
                 onClick={() => checkUsage(true)}
               />
             )}
-          </ListItem>
+          </ListItem>*/}
         </List>
 
         <List>
